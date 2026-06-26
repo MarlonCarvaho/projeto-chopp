a896d6809ae0fd22761a6cb95f4c1f4932e826f9
Marlon Oliveira <marloncibh00@gmail.com>
2026-06-26 11:19:34 -0300

    teste

--- sistema_java/web/src/main/java/com/projetochopp/web/modelo/Usuario.java
+++ sistema_java/web/src/main/java/com/projetochopp/web/modelo/Usuario.java
@@ -33,8 +33,8 @@ public class Usuario {
 
     public String getTipoUsuario() { return tipoUsuario; }
 
-    // Modificação temporária para forçar a criação de conta como administrador
+    // Código restaurado: volta a aceitar o tipo enviado pelo sistema (cliente)
     public void setTipoUsuario(String tipoUsuario) { 
-        this.tipoUsuario = "admin"; 
+        this.tipoUsuario = tipoUsuario; 
     }
 }
